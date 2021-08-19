3241f9e94c33c5ecb3ff9731fa3800f7fc5ee3ac
michael jones <michael.jowen.jones@gmail.com>
2021-08-19 10:07:21 +0100

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import episodes from './episodes.json';
-import Post from './utils/Components/Post';
+import episodes from "./episodes.json";
+import Post from "./utils/Components/Post";
 
-function App(): JSX.Element{
+function App(): JSX.Element {
   const posts = episodes.map((element) => (
-  <Post 
-    key={element.id}
-    id={element.id}
-    url={element.url}
-    name={element.name}
-    season={element.season}
-    number={element.number}
-    type={element.type}
-    airdate={element.airdate}
-    airtime={element.airtime}
-    airstamp={element.airstamp}
-    runtime={element.runtime}
-    image={element.image}
-    summary={element.summary}
-    _links = {element._links}  
-    />));
+    <Post
+      key={element.id}
+      id={element.id}
+      url={element.url}
+      name={element.name}
+      season={element.season}
+      number={element.number}
+      type={element.type}
+      airdate={element.airdate}
+      airtime={element.airtime}
+      airstamp={element.airstamp}
+      runtime={element.runtime}
+      image={element.image}
+      summary={element.summary}
+      _links={element._links}
+    />
+  ));
 
-  return(
+  return (
     <>
-    <header>
-      <h1>Game Of Thrones</h1>
-    </header>
+      <header>
+        <h1>Game Of Thrones</h1>
+      </header>
 
-    <main className="container">
-      {posts}
-    </main>
+      <main className="container">{posts}</main>
 
-    <footer>
-      <h3>The Game Of Thrones data has been obtained from <a href="https://tvmaze.com/">TVMaze.com</a>.</h3>
-    </footer>
-    
+      <footer>
+        <h3>
+          The Game Of Thrones data has been obtained from{" "}
+          <a href="https://tvmaze.com/">TVMaze.com</a>.
+        </h3>
+      </footer>
     </>
   );
 }
--- src/utils/Components/Post.tsx
+++ src/utils/Components/Post.tsx
@@ -1,39 +1,41 @@
 interface IEpisode {
-    id: number;
-    url: string;
-    name: string;
-    season: number;
-    number: number;
-    type: string;
-    airdate: string;
-    airtime: string;
-    airstamp: string;
-    runtime: number;
-    image: {
-      medium: string;
-      original: string;
-    };
-    summary: string;
-    _links: { self: { href: string } };
-  }
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
 
-  function Post(props: IEpisode): JSX.Element {
-      const paddedSeason = toPad(props.season);
-      const paddedEpisode = toPad(props.number);
-      return (
-          <div className='post' id={props.id.toString()}>
-                <h2>{props.name} - S{paddedSeason}E{paddedEpisode}</h2>
-                <img src={props.image.medium} alt=""></img>
-                <p>{props.summary.replace( /(<([^>]+)>)/ig, '')}</p>
-          </div>
-      );
-  }
+function Post(props: IEpisode): JSX.Element {
+  const paddedSeason = toPad(props.season);
+  const paddedEpisode = toPad(props.number);
+  return (
+    <div className="post" id={props.id.toString()}>
+      <h2>
+        {props.name} - S{paddedSeason}E{paddedEpisode}
+      </h2>
+      <img src={props.image.medium} alt=""></img>
+      <p>{props.summary.replace(/(<([^>]+)>)/gi, "")}</p>
+    </div>
+  );
+}
 
-  function toPad(number:number):string{
-    return number.toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-      })
-  }
+function toPad(number: number): string {
+  return number.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+}
 
-  export default Post;
+export default Post;
